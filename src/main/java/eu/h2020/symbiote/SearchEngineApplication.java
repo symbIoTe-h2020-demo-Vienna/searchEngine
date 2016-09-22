b9f9ff74590becd6cb65840bad5baefb8dde8412
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2016-09-22 11:00:21 +0200

    added spring configuration

--- src/main/java/eu/h2020/symbiote/SearchEngineApplication.java
+++ src/main/java/eu/h2020/symbiote/SearchEngineApplication.java
@@ -2,7 +2,12 @@ package eu.h2020.symbiote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Created by mateuszl on 22.09.2016.
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SearchEngineApplication {
 
