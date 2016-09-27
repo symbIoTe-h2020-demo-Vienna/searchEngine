0c2aeb7d81d0229ef3053409aebf560375ae8ca8
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2016-09-27 14:53:38 +0200

    mongo tryouts

--- src/main/java/eu/h2020/symbiote/repository/SensorRepository.java
+++ src/main/java/eu/h2020/symbiote/repository/SensorRepository.java
@@ -3,7 +3,6 @@ package eu.h2020.symbiote.repository;
 import eu.h2020.symbiote.model.Sensor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -13,8 +12,7 @@ import java.util.List;
  */
 
 @RepositoryRestResource(collectionResourceRel = "sensor", path = "sensor")
-public interface SensorRepository extends MongoRepository<Sensor, String>,
-        QueryDslPredicateExecutor<Sensor> {
+public interface SensorRepository extends MongoRepository<Sensor, String> {
 
     @Query("{'platform.id' : ?0 }")
     public List<Sensor> findByPlatformId(String platformId);
